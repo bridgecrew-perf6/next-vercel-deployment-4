a93ade36584bcdf384cde05d893e8be1ba170bea
pbermejo <pbermejo.dev@gmail.com>
2022-04-06 08:59:00 +0200

    Pre-deployment

---
+++ pages/contact/index.tsx
@@ -0,0 +1,14 @@
+import { MainLayout } from "../../components/layouts/MainLayout";
+
+export default function About() {
+	return (
+		<MainLayout>
+			<h1 className={'title'}>Contact Page</h1>
+
+			<p className={'description'}>
+				Get started by editing{" "}
+				<code className={'code'}>pages/contact(index).js</code>
+			</p>
+		</MainLayout>
+	);
+}
---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import { MainLayout } from '../components/layouts/MainLayout'
+
+export default function Home() {
+  return (
+    <MainLayout>
+      <h1 className={'title'}>Home Page</h1>
+
+      <p className={'description'}>
+        Get started by editing{' '}
+        <code className={'code'}>pages/index.js</code>
+      </p>
+    </MainLayout>
+  )
+}
